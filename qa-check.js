9330ae9bbf25aa13b4785ae438be77dfc2ba457d
sagorvai <82975730+sagorvai@users.noreply.github.com>
2026-08-09 11:47:07 +0600

    Delete qa-check.js

--- qa-check.js
+++
@@ -1,7 +0,0 @@
-// Run in browser console on each page during QA.
-(() => {
-  const required = ['firebase','db'];
-  console.table(required.map(k => ({name:k, ok: !!window[k]})));
-  console.log('Page:', location.pathname, 'Viewport:', innerWidth + 'x' + innerHeight);
-  console.log('CSS files:', [...document.querySelectorAll('link[rel="stylesheet"]')].map(x => x.href));
-})();
